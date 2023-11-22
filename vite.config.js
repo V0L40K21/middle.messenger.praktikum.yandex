24be78a106714bbcd6e22d89a58ecdaed851bc66
V0L40K <vol4ok94@gmail.com>
2023-11-22 23:00:38 +0600

    other pages

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,22 @@ import {defineConfig} from 'vite'
 import {handlebarsPlugin} from './hbs.config'
 import handlebars from './vite.plugin.precompile'
 
+const rollupOptions = {
+	input: {
+		main: resolve('./src/index.html'),
+		auth: resolve('./src/pages/auth/index.html'),
+		registration: resolve('./src/pages/registration/index.html'),
+		chats: resolve('./src/pages/chats/index.html'),
+		404: resolve('./src/pages/errors/404.html'),
+		500: resolve('./src/pages/errors/500.html')
+	}
+}
+
 export default defineConfig({
 	root: resolve(__dirname, 'src'),
 	build: {
 		outDir: resolve(__dirname, 'dist'),
-		rollupOptions: {
-			input: {
-				main: resolve('./src/index.html'),
-				404: resolve('./src/pages/errors/404.html'),
-				500: resolve('./src/pages/errors/500.html')
-			}
-		}
+		rollupOptions
 	},
 	plugins: [handlebarsPlugin, handlebars()]
 })
